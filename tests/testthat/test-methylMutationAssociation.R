a16e5313be23005abc2e0cb548831060cbd629ea
JuanDeLosRios <juan.delosrios@mail.utoronto.ca>
2022-12-06 22:14:37 -0500

    Updated test scripts to address incorrect inputs

--- tests/testthat/test-methylMutationAssociation.R
+++ tests/testthat/test-methylMutationAssociation.R
@@ -14,4 +14,29 @@ test_that("function generates the correct data and plot", {
   expect_type(result, "NULL")
 })
 
+test_that("incorrect inputs", {
+
+  empty_matrix <- matrix(, nrow = 0, ncol = 1)
+  empty_list <- list()
+
+  # empty datasets for methylation and gene data
+  expect_error(result <- methylMutationAssociation(
+    methylationCancer = empty_matrix,
+    methylationNormal = empty_matrix,
+    geneCancer = empty_matrix,
+    mutationData = empty_list))
+
+  library(MethylMix)
+  data("METcancer")
+  data("METnormal")
+  data("GEcancer")
+
+  # empty list of mutation data
+  expect_error(result <- methylMutationAssociation(
+    methylationCancer = METcancer,
+    methylationNormal = METnormal,
+    geneCancer = GEcancer,
+    mutationData = empty_list))
+})
+
 # [END]
--- tests/testthat/test-methylOverrep.R
+++ tests/testthat/test-methylOverrep.R
@@ -12,4 +12,27 @@ test_that("function generates the correct data and plot", {
   expect_type(plot, "list")
 })
 
+test_that("incorrect inputs", {
+
+  empty_matrix <- matrix(, nrow = 0, ncol = 1)
+  empty_list <- list()
+
+  expect_error(plot <- methylOverrep(methylationCancer = empty_matrix,
+                        methylationNormal = empty_matrix,
+                        geneCancer = empty_matrix,
+                        pValue = 0.05,
+                        qValue = 0.1))
+
+  library(MethylMix)
+  data("METcancer")
+  data("METnormal")
+  data("GEcancer")
+
+  expect_error(plot <- methylOverrep(methylationCancer = METcancer,
+                                     methylationNormal = METnormal,
+                                     geneCancer = GEcancer,
+                                     pValue = 2,
+                                     qValue = 2))
+})
+
 # [END]
