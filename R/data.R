137da28e162c0f03a1e28115336bf45bcd225033
JuanDeLosRios <juan.delosrios@mail.utoronto.ca>
2022-12-07 19:14:50 -0500

    Provided more detail in example dataset to help users in creating their own mutation lists

--- R/data.R
+++ R/data.R
@@ -1,6 +1,9 @@
 #' Gene-Mutation list
 #'
-#' An example of a list of genes filed under their respective mutation categories
+#' An example of a list of genes filed under their respective mutation
+#' categories. The example is an R list object where the category each gene
+#' belongs to is defined in the names of the List. Each entry in the List is a
+#' vector of characters containing a series of gene names.
 #'
 #' @name mutationExample
 #' @examples
